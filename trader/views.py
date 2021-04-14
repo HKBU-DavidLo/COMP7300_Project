91eeffba42e29bb6dc20a29ee1df00959255a931
@jasonmesserfs <73806799+jasonmesserfs@users.noreply.github.com>
2021-04-14 17:53:36 +0800

    Update views.py

--- trader/views.py
+++ trader/views.py
@@ -55,17 +55,23 @@ def dashboard(request):
     stocks = StockHoldings.objects.filter(user=request.user)
     stocks_update = []
     stock_update = {}
+    total_pl = 0
     for stock in stocks:
         stock_update = {
             'symbol': stock.symbol,
             'quantity': stock.quantity,
             'avg_purchase_price': stock.avg_purchase_price
         }
-        stock_update['current'] = finnhub_client.quote(stock.symbol)['c']
+        mkt_price = finnhub_client.quote(stock.symbol)['c']
+        pl = (mkt_price - stock.avg_purchase_price)*stock.quantity
+        stock_update['current'] = mkt_price
+        stock_update['profit_loss'] = round(pl,2)
         stocks_update.append(stock_update)
+        total_pl += round(pl, 2)
     context = {
         'username': username,
         'cash': cash.cash,
         'stocks': stocks_update,
+        'total_pl': total_pl,
     }
     return render(request, 'dashboard.html', context)

3801412b84303b98882f089c0059bd6111dc159a
@jasonmesserfs <70503745+JasonWong11@users.noreply.github.com>
2021-04-14 18:31:19 +0800

    Portfolio

--- trader/admin.py
+++ trader/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Transaction, Stock
-# Register your models here.
+from .models import Stock, Transaction, Cash, StockHoldings
+
 admin.site.register(Stock)
 admin.site.register(Transaction)
+admin.site.register(Cash)
+admin.site.register(StockHoldings) 
--- trader/views.py
+++ trader/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponseRedirect, JsonResponse
+from django.http import HttpResponseRedirect, JsonResponse, HttpResponse
 from django.utils.timezone import now
 from django.urls import reverse
 from .forms import BuyShareForm
@@ -9,7 +9,13 @@ from django.contrib.auth.decorators import login_required
 import requests
 import datetime
 import logging
+from .models import Cash, StockHoldings
+from django.template import loader
+from django.shortcuts import render
+import finnhub
 
+# Setup client
+finnhub_client = finnhub.Client(api_key="c0p7jnn48v6rvej4esog")
 
 def index(request):
     if request.user.is_authenticated:
@@ -61,11 +67,29 @@ def orderpreview(request):
 
 @login_required
 def dashboard(request):
-    act_fund = 12410
-    username = 'Username'
+    cash = Cash.objects.get(user=request.user)
+    username = request.user
+    stocks = StockHoldings.objects.filter(user=request.user)
+    stocks_update = []
+    stock_update = {}
+    total_pl = 0
+    for stock in stocks:
+        stock_update = {
+            'symbol': stock.symbol,
+            'quantity': stock.quantity,
+            'avg_purchase_price': stock.avg_purchase_price
+        }
+        mkt_price = finnhub_client.quote(stock.symbol)['c']
+        pl = (mkt_price - stock.avg_purchase_price)*stock.quantity
+        stock_update['current'] = mkt_price
+        stock_update['profit_loss'] = round(pl,2)
+        stocks_update.append(stock_update)
+        total_pl += round(pl, 2)
     context = {
         'username': username,
-        'act_fund': act_fund,
+        'cash': cash.cash,
+        'stocks': stocks_update,
+        'total_pl': total_pl,
     }
     return render(request, 'dashboard.html', context)
 

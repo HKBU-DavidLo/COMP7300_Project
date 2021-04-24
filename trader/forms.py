abb0534d7b9ace21b5e9cfcad95d8110186cd9f0
David Lo <19451415@life.hkbu.edu.hk>
2021-04-24 15:24:38 +0800

    buy sell remove stock restriction, frontend validate

--- trader/forms.py
+++ trader/forms.py
@@ -4,16 +4,16 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 
 class ShareTransactionForm(forms.Form):
-    SHARE_CHOICES = [
-        ('AAPL', 'Apple (AAPL)'),
-        ('GOOG', 'Google (GOOG)'),
-        ('BUD', 'AB InBev (ABI)'),
-        ('AMZN', 'Amazon (AMZN)'),
-        ('BTI', 'BA Tobacco (BTI)'),
-        ('ET', 'Energy Transfer (ET)'),
-        ('NI', 'NiSource (NI)'),
-    ] 
-    stock = forms.ChoiceField(label='Select a stock', choices=SHARE_CHOICES)
+    # SHARE_CHOICES = [
+    #     ('AAPL', 'Apple (AAPL)'),
+    #     ('GOOG', 'Google (GOOG)'),
+    #     ('BUD', 'AB InBev (ABI)'),
+    #     ('AMZN', 'Amazon (AMZN)'),
+    #     ('BTI', 'BA Tobacco (BTI)'),
+    #     ('ET', 'Energy Transfer (ET)'),
+    #     ('NI', 'NiSource (NI)'),
+    # ] 
+    stock = forms.CharField(label='Enter a symbol', widget=forms.TextInput(attrs={'required': "required"}))
     quantity = forms.IntegerField(label='Quantity')
     limit_price = forms.FloatField(label='Limit order price (US$)')
     tx_time = forms.DateField(widget=forms.HiddenInput(), required=False)
